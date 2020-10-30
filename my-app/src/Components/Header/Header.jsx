a1bc718d7cb705f5fe74c4392974a80636bb5d27
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-10-30 02:34:13 +0200

    svg fix

--- my-app/src/Components/Header/Header.jsx
+++ my-app/src/Components/Header/Header.jsx
@@ -6,11 +6,11 @@ const Header = () => {
         <header className={css.header}>
             <div className={css.header__logo}></div>
             <div className={css.header__basket}></div>
-            <div className={css.header__btn}>
-                <button className={css.header__btn_language_ua}></button>
+            <div className={css.header__shoppingCart}></div>
+            <div className={css.header__btn_hamburger}></div>
+             {/* <button className={css.header__btn_language_ua}></button>
                 <button className={css.header__btn_language_ru}></button>
-                <button className={css.header__btn_language_en}></button>
-            </div>
+                <button className={css.header__btn_language_en}></button> */}
         </header>
     )
 
