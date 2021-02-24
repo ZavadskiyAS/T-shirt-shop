bfeae22dc340c2207a9188940f8cfe75d345dc5d
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-02-25 00:24:00 +0200

    adaptive slider v2

--- src/index.js
+++ src/index.js
@@ -8,6 +8,34 @@ let width;
 function init(){
     console.log('resize');
     width = document.querySelector('.slider').offsetWidth;
+    sliderLine.style.width = width*images.length + 'px';
+    images.forEach( item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    });
     console.log('width :>> ', width);
+    rollSlider();
 }
+
+window.addEventListener('resize', init);
 init();
+
+document.querySelector('.slider__btnNext').addEventListener('click', function(){
+    count++;
+    if (count >= images.length ) {
+        count = 0;
+    }
+    rollSlider();
+})
+
+document.querySelector('.slider__btnPrev').addEventListener('click', function(){
+    count--;
+    if (count < 0 ) {
+        count = images.length -1;
+    }
+    rollSlider();
+})
+
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-' +count * width + 'px)';
+}
