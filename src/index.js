5747f1386f27f816659d89153b1bed132ce05e17
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-02-24 23:30:12 +0200

    Adaptive slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+
+const images = document.querySelectorAll('.slider .slider__line img');
+const sliderLine = document.querySelector('.slider__line');
+let count = 0;
+let width;
+
+function init(){
+    console.log('resize');
+    width = document.querySelector('.slider').offsetWidth;
+    console.log('width :>> ', width);
+}
+init();
